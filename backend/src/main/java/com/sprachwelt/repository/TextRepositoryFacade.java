4c7272ee543605bd65c04cc0a98b6f294f4e1a19
Ivan Gandacov <gandacov96@gmail.com>
2020-10-07 09:06:02 +0200

    [backend] Add method for getting mapping of word and its positions in text

--- backend/src/main/java/com/sprachwelt/repository/TextRepositoryFacade.java
+++ backend/src/main/java/com/sprachwelt/repository/TextRepositoryFacade.java
@@ -4,8 +4,16 @@ import com.sprachwelt.model.Text;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Component
 public class TextRepositoryFacade {
+
+    @PersistenceContext
+    EntityManager entityManager;
 
     @Autowired
     private TextRepository textRepository;
@@ -13,5 +21,23 @@ public class TextRepositoryFacade {
     public Text save(Text text) {
         text.getWords().forEach(word -> word.setText(text));
         return this.textRepository.save(text);
+    }
+
+    public Map<String, Set<Integer>> getWord2PositionMappings(Long textId) {
+        List<Object[]> list = entityManager.createNativeQuery("select content, array_to_string(array_agg(position), ',') as positions\n" +
+                "from word\n" +
+                "where text_id = \n" + textId +
+                "group by content").getResultList();
+
+        Map<String, Set<Integer>> word2PositionMap = new HashMap<>();
+
+        for (Object[] result : list) {
+
+            Set<Integer> positions = Arrays.asList(((String) result[1]).split(",")).stream().map(s -> Integer.parseInt(s))
+                    .collect(Collectors.toSet());
+
+            word2PositionMap.put((String) result[0], positions);
+        }
+        return word2PositionMap;
     }
 }
