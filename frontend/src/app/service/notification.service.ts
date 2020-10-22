715820e76d700b0ba13cbf682d9052da09bda893
Ivan Gandacov <gandacov96@gmail.com>
2020-10-22 08:05:40 +0200

    [frontend] Refactor NotificationService

--- frontend/src/app/service/notification.service.ts
+++ frontend/src/app/service/notification.service.ts
@@ -1,30 +1,77 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService {
-  private SUCCESS_SNACKBAR_DURATION = 5000;
+  private SUCCESS_SNACKBAR_DURATION = 3000;
   private lastMessageId;
+  private messageIsStillVisible = false;
+  private dismissedSubscription: Subscription;
+  private openedSubscription: Subscription;
 
   constructor(private snackBar: MatSnackBar) {}
 
   showSuccess(message: string): void {
-    this.snackBar.open(message, '', {
-      duration: this.SUCCESS_SNACKBAR_DURATION,
-    });
+    return this.showMessage(message, this.showSuccessMessage.bind(this));
   }
 
-  showError(message: string): string {
-    this.snackBar.open(message, 'X', { panelClass: ['error'] });
-    this.lastMessageId = Date.now().toString();
-    return this.lastMessageId;
+  showError(message: string) {
+    return this.showMessage(message, this.showErrorMessage.bind(this));
   }
 
   dismissError(messageId: string) {
     if (messageId === this.lastMessageId) {
       this.snackBar.dismiss();
     }
+  }
+
+  private showMessage(message: string, showFunc) {
+    const messageId = this.getId(message);
+    if (messageId !== this.lastMessageId || !this.messageIsStillVisible) {
+      this.lastMessageId = messageId;
+      showFunc(message);
+    }
+    return this.lastMessageId;
+  }
+
+  private addSnackBarListeners() {
+    this.clearSnackbarSubscriptions();
+    this.dismissedSubscription = this.snackBar._openedSnackBarRef
+      .afterDismissed()
+      .subscribe(() => {
+        this.messageIsStillVisible = false;
+      });
+    this.openedSubscription = this.snackBar._openedSnackBarRef
+      .afterOpened()
+      .subscribe(() => {
+        this.messageIsStillVisible = true;
+      });
+  }
+
+  clearSnackbarSubscriptions() {
+    if (this.dismissedSubscription || this.openedSubscription) {
+      this.dismissedSubscription.unsubscribe();
+      this.openedSubscription.unsubscribe();
+    }
+  }
+
+  private showSuccessMessage(message: string) {
+    this.snackBar.open(message, '', {
+      duration: this.SUCCESS_SNACKBAR_DURATION,
+    });
+    this.addSnackBarListeners();
+  }
+
+  private showErrorMessage(message: string) {
+    this.snackBar.open(message, 'X', { panelClass: ['error'] });
+    this.addSnackBarListeners();
+  }
+
+  private getId(message: string) {
+    return Md5.hashStr(message);
   }
 }
