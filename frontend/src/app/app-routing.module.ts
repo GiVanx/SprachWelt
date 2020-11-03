c7cce7072d899afb636f6bea308ad1aaeecdf552
Ivan Gandacov <gandacov96@gmail.com>
2020-11-03 08:11:56 +0100

    [fe] Change login redirect logic

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -16,9 +16,6 @@ const routes: Routes = [
       {
         path: '',
         component: HomeComponent,
-        resolve: {
-          activeGame: ActiveGameResolverService,
-        },
       },
     ],
   },
